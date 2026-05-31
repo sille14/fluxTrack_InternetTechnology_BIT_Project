d96689481f9c1315d47b879559a333f9583c7f7e
Remy Brunner <remy.brunner@students.fhnw.ch>
2026-05-31 21:07:40 +0200

    added /welcome to permitAll to do sanity check without auth

--- fluxTrack/src/main/java/ch/fluxed/fluxtrack/security/SecurityConfig.java
+++ fluxTrack/src/main/java/ch/fluxed/fluxtrack/security/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                     "/",
+                    "/welcome",
                     "/login",
                     "/products",
                     "/partners",
